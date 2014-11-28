b4b779ada4723b31d5532f66b44f7e5d49d5845b
magiknono <arno.cormier@gmail.com>
2014-11-28 11:20:32 +0100

    first version finished,to do destroy

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,28 @@
 class DosesController < ApplicationController
 
   def new
-    @dose = Dose.new
+    # Dose.create(amount:5, cocktail: mojito)
+    @dose = Dose.new(cocktail: @cocktail)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @ingredients = Ingredient.all
 
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  def dose_params
+    params.require(:dose).permit(:amount, :ingredient_id)
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :cocktails, through: :doses
   validates :name, presence: true
+  # validates_presence_of :name #avantageux pour en ajouter plusieurs
 end
